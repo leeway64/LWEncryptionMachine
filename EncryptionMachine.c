e347842aa2ba5f10e748668fe3edfba0c48e6eb6
Lee-Way Wang <lee.way56@comcast.net>
2021-09-20 19:40:31 -0700

    gitignore, README, printIntro
    - Update gitignore and README
    - Print introduction using printIntro

--- EncryptionMachine.c
+++ EncryptionMachine.c
@@ -1,12 +1,30 @@
 //
 
+#include <stdio.h>
+#include <stdlib.h>
+
+const char* ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+#define SHIFT 3
+
+
+void printIntro() {
+	char* intro = "";
+	sprintf(intro, "Welcome to the CSE142 Encryption Machine\n");
+	sprintf(intro + strlen(intro), "The program lets you encrypt a message\n");
+	sprintf(intro + strlen(intro), "with a key for your recipient to decrypt!\n\n");
+	puts(intro);
+}
+
+/*@param */
 void encryptLetter(char letter) {
 
 }
 
+/*@param */
 void encryptWord(char word[]) {
 }
 
 int main() {
+	printIntro();
 	return 0;
 }
--- README.rst
+++ README.rst
@@ -1,3 +1,6 @@
 Encryption Machine
 ===================
 
+This project is based on `Assessment 3 <https://courses.cs.washington.edu/courses/cse142/21su/assessments/a3/>`_
+for `UW Seattle CSE 142 (summer 2020) <https://courses.cs.washington.edu/courses/cse142/21su/>`_.
+The assignment was originally meant to be completed in Java, but I completed it in C.
