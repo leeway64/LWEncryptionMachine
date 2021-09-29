21f9eba7ecb60d418d5c23ac1d0819c654b2b38a
Lee-Way Wang <lee.way56@comcast.net>
2021-09-28 20:12:12 -0700

    Begin writing unit tests, create test_suite.exe

--- tests/README.rst
+++ tests/README.rst
@@ -1,5 +1,7 @@
 Unit Tests
 ==============
 
-To run unit tests, enter cl test_suite.c CuTest.c /link /out:test_suite.exe into the developer
+To run the unit tests, enter cl test_suite.c CuTest.c /link /out:test_suite.exe into the developer
 command prompt for VS in the current directory to compile tests. Then, type test_suite.exe to run tests.
+
+Alternatively, select test_suite.exe as the target and run that executable.
--- tests/test_suite.c
+++ tests/test_suite.c
@@ -1,15 +1,50 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "CuTest.h"
 #include "../EncryptionMachine.h"
 
 
 void testIndexOf(CuTest* tc) {
-    CuAssertStrEquals(tc, "hello", "hello");
+    CuAssertIntEquals(tc, indexOf('a'), 0);
+    CuAssertIntEquals(tc, indexOf('b'), 1);
+    CuAssertIntEquals(tc, indexOf('c'), 2);
+    CuAssertIntEquals(tc, indexOf('s'), 18);
+    CuAssertIntEquals(tc, indexOf('x'), 23);
+    CuAssertIntEquals(tc, indexOf('y'), 24);
+    CuAssertIntEquals(tc, indexOf('z'), 25);
 }
 
-CuSuite* StrUtilGetSuite() {
+void testEncryptLetter(CuTest* tc) {
+    CuAssertIntEquals(tc, encryptLetter('a'), 'd');
+    CuAssertIntEquals(tc, encryptLetter('d'), 'g');
+    CuAssertIntEquals(tc, encryptLetter('e'), 'h');
+    CuAssertIntEquals(tc, encryptLetter('f'), 'i');
+    CuAssertIntEquals(tc, encryptLetter('n'), 'q');
+    CuAssertIntEquals(tc, encryptLetter('y'), 'b');
+    CuAssertIntEquals(tc, encryptLetter('z'), 'c');
+
+}
+
+void testCopyString(CuTest* tc) {
+    
+}
+
+void testEncryptWord(CuTest* tc) {
+    /*CuAssertStrEquals(tc);
+    CuAssertStrEquals(tc);
+    CuAssertStrEquals(tc);
+    CuAssertStrEquals(tc);
+    CuAssertStrEquals(tc);
+    CuAssertStrEquals(tc);*/
+
+}
+
+CuSuite* EncryptionMachineGetSuite() {
     CuSuite* suite = CuSuiteNew();
     SUITE_ADD_TEST(suite, testIndexOf);
+    SUITE_ADD_TEST(suite, testEncryptLetter);
+    SUITE_ADD_TEST(suite, testEncryptWord);
+    SUITE_ADD_TEST(suite, testCopyString);
     return suite;
 }
 
@@ -17,7 +52,7 @@ void RunAllTests(void) {
     CuString* output = CuStringNew();
     CuSuite* suite = CuSuiteNew();
 
-    CuSuiteAddSuite(suite, StrUtilGetSuite());
+    CuSuiteAddSuite(suite, EncryptionMachineGetSuite());
 
     CuSuiteRun(suite);
     CuSuiteSummary(suite, output);
